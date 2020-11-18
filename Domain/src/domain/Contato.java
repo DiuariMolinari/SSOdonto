851af811433a29f9105e656474b3e29c3ae18f87
marcieleadivincula <marcieleadivincula@gmail.com>
2020-11-18 20:55:26 -0300

    Adicionando getters and setters da classe Contato

--- Domain/src/domain/Contato.java
+++ Domain/src/domain/Contato.java
@@ -7,12 +7,53 @@ package domain;
 
 /**
  *
- * @author Diuari
+ * @author Diuari e Marciele
  */
 public class Contato {
+
     private int id;
     private String fone;
     private FoneTipo foneTipo;
     private String email;
     private Paciente paciente;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public FoneTipo getFoneTipo() {
+        return foneTipo;
+    }
+
+    public void setFoneTipo(FoneTipo foneTipo) {
+        this.foneTipo = foneTipo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
 }
