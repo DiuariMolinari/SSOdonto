3789e9d083d8d16589677cbf306930bc6a84d63e
marcieleadivincula <marcieleadivincula@gmail.com>
2020-11-20 23:59:07 -0300

    Criado classe UsuarioBLL e seus verificações

---
+++ BusinessLogicalLayer/src/businessLogicalLayer/UsuarioBLL.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogicalLayer;
+
+import dataAccessLayer.UsuarioDAL;
+import domain.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author Marciele
+ */
+public class UsuarioBLL {
+
+    UsuarioDAL dal = new UsuarioDAL();
+
+    //Incluir um registro
+    public String Inserir(Usuario usuario) {
+        StringBuilder builder = new StringBuilder();
+
+        if (usuario.getUsuario().isBlank() || usuario.getUsuario().isEmpty() || usuario.getUsuario().length() == 0) {
+            builder.append("O usuário de login deve ser informado.");
+        }
+        if (usuario.getUsuario().length() > 60) {
+            builder.append("O usuário de login não pode conter mais que 60 caracteres.");
+        }
+
+        if (usuario.getSenha().isBlank() || usuario.getSenha().isEmpty() || usuario.getSenha().length() == 0) {
+            builder.append("A senha deve ser informada.");
+        }
+        if (usuario.getSenha().length() > 250) {
+            builder.append("A senha não pode conter mais que 250 caracteres.");
+        }
+
+        if (builder.length() != 0) {
+            return builder.toString();
+        }
+        String respostaDB = dal.Inserir(usuario);
+        return respostaDB;
+    }
+
+//    public void Autenticar(String login, String password) {
+//        Usuario user = dal.Autenticar(login, password);
+//        if (user == null) {
+//            throw new Exception("Usuário e/ou senha inválidos.");
+//        }
+//        Parametros.UsuarioLogado = user;
+//    }
+
+    // Obter todos os registros
+    public List<Usuario> LerTodos() {
+        return dal.LerTodos();
+    }
+
+    //Atualizar um registro existente
+    public String Atualizar(Usuario usuario) {
+        StringBuilder builder = new StringBuilder();
+
+        if (usuario.getUsuario().isBlank() || usuario.getUsuario().isEmpty() || usuario.getUsuario().length() == 0) {
+            builder.append("O usuário de login deve ser informado.");
+        }
+        if (usuario.getUsuario().length() > 60) {
+            builder.append("O usuário de login não pode conter mais que 60 caracteres.");
+        }
+
+        if (usuario.getSenha().isBlank() || usuario.getSenha().isEmpty() || usuario.getSenha().length() == 0) {
+            builder.append("A senha deve ser informada.");
+        }
+        if (usuario.getSenha().length() > 250) {
+            builder.append("A senha não pode conter mais que 250 caracteres.");
+        }
+
+        String respostaDB = dal.Atualizar(usuario);
+        return respostaDB;
+    }
+
+    //Excluir um registro
+    public String Deletar(Usuario usuario) {
+        String respostaDB = dal.Deletar(usuario);
+        return respostaDB;
+    }
+
+    //TODO: Obter um registro
+}
