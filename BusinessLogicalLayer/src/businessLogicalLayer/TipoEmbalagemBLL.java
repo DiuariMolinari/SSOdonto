57518f2117eaa48e0b36ad9792a836cfdf845a21
marcieleadivincula <marcieleadivincula@gmail.com>
2020-11-20 22:21:36 -0300

    Criado classe TipoEmbalagemBLL e seus verificações

---
+++ BusinessLogicalLayer/src/businessLogicalLayer/TipoEmbalagemBLL.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogicalLayer;
+
+import dataAccessLayer.TipoEmbalagemDAL;
+import domain.TipoEmbalagem;
+import java.util.List;
+
+/**
+ *
+ * @author Marciele
+ */
+public class TipoEmbalagemBLL {
+
+    TipoEmbalagemDAL dal = new TipoEmbalagemDAL();
+
+    //Incluir um registro
+    public String Inserir(TipoEmbalagem tipoEmbalagem) {
+        StringBuilder builder = new StringBuilder();
+
+        if (tipoEmbalagem.getDescricao().isBlank() || tipoEmbalagem.getDescricao().isEmpty() || tipoEmbalagem.getDescricao().length() == 0) {
+            builder.append("A descricao deve ser informado.");
+        }
+
+        if (tipoEmbalagem.getDescricao().length() > 60) {
+            builder.append("A descricao não pode conter mais que 60 caracteres.");
+        }
+
+        if (builder.length() != 0) {
+            return builder.toString();
+        }
+        String respostaDB = dal.Inserir(tipoEmbalagem);
+        return respostaDB;
+    }
+
+    // Obter todos os registros
+    public List<TipoEmbalagem> LerTodos() {
+        return dal.LerTodos();
+    }
+
+    //Atualizar um registro existente
+    public String Atualizar(TipoEmbalagem tipoEmbalagem) {
+        StringBuilder builder = new StringBuilder();
+
+        if (tipoEmbalagem.getDescricao().isBlank() || tipoEmbalagem.getDescricao().isEmpty() || tipoEmbalagem.getDescricao().length() == 0) {
+            builder.append("A descricao deve ser informado.");
+        }
+
+        if (tipoEmbalagem.getDescricao().length() > 60) {
+            builder.append("A descricao não pode conter mais que 60 caracteres. ");
+        }
+
+        String respostaDB = dal.Atualizar(tipoEmbalagem);
+        return respostaDB;
+    }
+
+    //Excluir um registro
+    public String Deletar(TipoEmbalagem tipoEmbalagem) {
+        String respostaDB = dal.Deletar(tipoEmbalagem);
+        return respostaDB;
+    }
+
+    //TODO: Obter um registro  
+}
