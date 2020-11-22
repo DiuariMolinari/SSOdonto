3ad6acaaa840f07554bd840f132e45e40a37a8d1
Sabrina Blockwel <sblockwel@gmail.com>
2020-11-22 17:35:14 -0300

    Adicionado formulario de cadastro de endereço

--- PresentationLayer/src/presentationlayer/FormCadastroTipoProcedimento.java
+++ PresentationLayer/src/presentationlayer/FormCadastroTipoProcedimento.java
@@ -9,12 +9,12 @@ package presentationlayer;
  *
  * @author sabri
  */
-public class FormTipoProcedimento extends javax.swing.JFrame {
+public class FormCadastroTipoProcedimento extends javax.swing.JFrame {
 
     /**
      * Creates new form FormTipoProcedimento
      */
-    public FormTipoProcedimento() {
+    public FormCadastroTipoProcedimento() {
         initComponents();
     }
 
@@ -71,7 +71,7 @@ public class FormTipoProcedimento extends javax.swing.JFrame {
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(14, 14, 14))
@@ -131,20 +131,21 @@ public class FormTipoProcedimento extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FormTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormCadastroTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FormTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormCadastroTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FormTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormCadastroTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FormTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FormCadastroTipoProcedimento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FormTipoProcedimento().setVisible(true);
+                new FormCadastroTipoProcedimento().setVisible(true);
             }
         });
     }
